673cb19c59d8241d1aafac112afcbf5b5b73a801
kvmanohar22 <kvmanohar22@gmail.com>
2017-10-18 03:33:49 +0530

    complete experiments on vae

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # Comparison of Generative Models in Tensorflow
 
 The different generative models considered here are **VAEs** and **GANs**
+
+This experiment is accompanied by blog post at : [https://kvmanohar22.github.io/Generative-Models](https://kvmanohar22.github.io/Generative-Models)
+
+## Usage
+
+- Download the MNIST and CIFAR datasets
+
+#### Train VAE on mnist by running:
+```python
+
+python --model vae --dataset mnist
+```
+#### Train GAN on mnist by running:
+
+```python
+
+python --model gan --dataset mnist
+```
+
+For the complete list of command line options, run:
+
+```python
+python main.py --help
+```
+
+The model generates images at a frequence specified by `generate_frq` which is by default 1.
--- utils/__init__.py
+++ utils/__init__.py
@@ -4,32 +4,54 @@ import numpy as np
 from skimage import io
 from scipy.misc import imsave
 
+from tensorflow.examples.tutorials.mnist import input_data
+
 class Dataset(object):
-	def __init__(self):
-		self.opts = Options()
-		imgs_file = self.opts.root_path+'/data_batch_1'
-		with open(imgs_file, 'rb') as f:
-			dict = cPickle.load(f)
-		imgs = dict['data']
-		self.images = np.reshape(imgs, (10000, 3, 32, 32)).transpose(0, 2, 3, 1).astype('uint8') / 127.5 - 1.
-		self.test_images = np.zeros((64, 32, 32, 3))
-		np.random.shuffle(self.images)
-		self.test_images = self.images[:64, :, :, :]
+	def __init__(self, opts):
+		self.opts = opts
+
+		if self.opts.dataset == 'CIFAR':
+			imgs_file = self.opts.root_dir+self.opts.dataset_dir+'data_batch_2'
+			with open(imgs_file, 'rb') as f:
+				dict = cPickle.load(f)
+			imgs = dict['data']
+			self.images = np.reshape(imgs, (10000, 3, 32, 32)).transpose(0, 2, 3, 1).astype('uint8')
+			self.test_images = np.zeros((self.opts.test_size, 32, 32, 3))
+			self.images = self.images.astype('float')
+			self.images /= 255.0
+			np.random.shuffle(self.images)
+			self.test_images = self.images[:self.opts.test_size, :, :, :]
+		else:
+			data_dir = self.opts.root_dir + self.opts.dataset_dir
+			mnist = input_data.read_data_sets(data_dir, one_hot=True)
+			images = mnist.train.images
+			self.images = np.reshape(images, (55000, 28, 28, 1)).astype('float')
+			self.test_images = np.zeros((self.opts.test_size, 28, 28, 1))
+			np.random.shuffle(self.images)
+			self.test_images = self.images[:self.opts.test_size, :, :]
 		self.save_batch_images(self.test_images, [8, 8], "target.jpg")
 
 	def load_batch(self, start_idx, end_idx):
 		return self.images[start_idx:end_idx]
 
-	def save_batch_images(self, images, grid, img_file):
+	def save_batch_images(self, images, grid, img_file, normalized=False):
 		h = images.shape[1]
 		w = images.shape[2]
+		if normalized:
+			images = images * 255.0
 		num = images.shape[0]
-		imgs = np.zeros((h * grid[0], h * grid[1], 3))
+		if self.opts.dataset == "CIFAR":
+			imgs = np.zeros((h * grid[0], h * grid[1], self.opts.channels))
+		else:
+			imgs = np.zeros((h * grid[0], h * grid[1]))
 
 		for idx, image in enumerate(images):
 			i = idx % grid[1]
 			j = idx / grid[1]
-			imgs[i*w:w*(i+1), j*h:(j+1)*h, :] = image
-		imsave(self.opts.base_img_dir+img_file, imgs)
-		with open(self.opts.base_img_dir+img_file.split('.')[0]+'-pkl', 'wb') as f:
-			cPickle.dump(imgs, f)
+			if self.opts.dataset == "CIFAR":
+				imgs[i*w:w*(i+1), j*h:(j+1)*h, :] = image
+			else:
+				imgs[i*w:w*(i+1), j*h:(j+1)*h] = np.reshape(image, (28, 28))
+		imsave(self.opts.root_dir+self.opts.sample_dir+img_file, imgs)
+		# with open(self.opts.root_dir+self.opts.sample_dir+img_file.split('.')[0]+'-pkl', 'wb') as f:
+		# 	cPickle.dump(imgs, f)
